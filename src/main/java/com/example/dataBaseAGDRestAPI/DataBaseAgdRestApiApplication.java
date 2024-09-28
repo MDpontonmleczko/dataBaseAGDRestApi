5ea2fb88ea4fc77459abf607ab04f2778ca1d743
Milosz Dudzik <miloszdudzik@gmail.com>
2024-09-28 20:18:28 +0200

    Creating new mvn project

---
+++ src/main/java/com/example/dataBaseAGDRestAPI/DataBaseAgdRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataBaseAGDRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseAgdRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseAgdRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dataBaseAGDRestAPI/DataBaseAgdRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataBaseAGDRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseAgdRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
