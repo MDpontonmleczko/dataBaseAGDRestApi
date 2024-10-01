34328aa780a11713cbd8f9808c5208a774313231
Milosz Dudzik <miloszdudzik@gmail.com>
2024-10-01 15:19:56 +0200

    Updated config file and application.properties

--- src/main/java/com/example/dataBaseAGDRestAPI/hibernate/HibernateConfig.java
+++ src/main/java/com/example/dataBaseAGDRestAPI/hibernate/HibernateConfig.java
@@ -5,7 +5,12 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -13,7 +18,21 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.example.dataBaseAGDRestAPI.appliance")
 public class HibernateConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.example.dataBaseAGDRestAPI.appliance");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
@@ -35,12 +54,18 @@ public class HibernateConfig {
         return dataSource;
     }
 
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty(
                 "hibernate.hbm2ddl.auto", "create-drop");
-        hibernateProperties.setProperty(
-                "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         hibernateProperties.setProperty(Environment.SHOW_SQL, "true");
 
         return hibernateProperties;
