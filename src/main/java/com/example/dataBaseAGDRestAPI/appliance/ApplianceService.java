1bcc8cefc48613021c0c9edbd5d15a8bc5cf83b8
Milosz Dudzik <miloszdudzik@gmail.com>
2024-10-01 15:17:19 +0200

    Added findAll (lists all positions from db) and findById (shows position from db by id) methods to ApplianceService

---
+++ src/main/java/com/example/dataBaseAGDRestAPI/appliance/ApplianceService.java
@@ -0,0 +1,38 @@
+package com.example.dataBaseAGDRestAPI.appliance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ApplianceService {
+
+    ApplianceRepository applianceRepository;
+
+    @Autowired
+    public ApplianceService(ApplianceRepository applianceRepository){
+        this.applianceRepository = applianceRepository;
+    }
+
+    public Appliance findById(Integer id){
+        Optional<Appliance> optionalAppliance = applianceRepository.findById(id);
+        return optionalAppliance.orElse(new Appliance("brak", "brak", 0));
+    }
+
+    public List<Appliance> findAll(){
+        ArrayList<Appliance> appliances = new ArrayList<>();
+
+        for(Appliance appliance : applianceRepository.findAll()) {
+            appliances.add(appliance);
+        }
+
+        return appliances;
+    }
+
+
+
+
+}
