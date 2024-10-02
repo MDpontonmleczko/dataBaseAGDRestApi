93fc89c14018556e3da0ff4a36dcf0dedf28b5a2
Milosz Dudzik <miloszdudzik@gmail.com>
2024-10-02 10:50:14 +0200

    Added addAppliance mapping

--- src/main/java/com/example/dataBaseAGDRestAPI/appliance/ApplianceController.java
+++ src/main/java/com/example/dataBaseAGDRestAPI/appliance/ApplianceController.java
@@ -3,6 +3,7 @@ package com.example.dataBaseAGDRestAPI.appliance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,4 +28,10 @@ public class ApplianceController {
         return applianceService.findAll();
     }
 
+    @PostMapping(value = "/add/appliance/{name}/{description}/{value}")
+    public String addAppliance(@PathVariable("name") String name,
+                               @PathVariable("description") String description,
+                               @PathVariable("value") double value){
+        return applianceService.addAppliance(name, description, value);
+    }
 }
